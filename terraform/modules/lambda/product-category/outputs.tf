5ea586c28e5afff61908f135aa9ba9df7fa9d892
Henrique Hartwig <henrique2805@gmail.com>
2025-04-27 22:05:13 -0300

    Refactor Lambda function outputs in Terraform module to use a dynamic approach for function details, improving maintainability. Added output for IAM role ARN used by the Lambda functions for better resource management.

--- terraform/modules/lambda/product-category/outputs.tf
+++ terraform/modules/lambda/product-category/outputs.tf
@@ -1,30 +1,15 @@
 output "lambda_functions" {
-  description = "Information about the product category lambda functions"
   value = {
-    create_product_category = {
-      name       = aws_lambda_function.product_category_functions["create_product_category"].function_name
-      arn        = aws_lambda_function.product_category_functions["create_product_category"].arn
-      invoke_arn = aws_lambda_function.product_category_functions["create_product_category"].invoke_arn
-    }
-    get_product_category = {
-      name       = aws_lambda_function.product_category_functions["get_product_category"].function_name
-      arn        = aws_lambda_function.product_category_functions["get_product_category"].arn
-      invoke_arn = aws_lambda_function.product_category_functions["get_product_category"].invoke_arn
-    }
-    list_product_categories = {
-      name       = aws_lambda_function.product_category_functions["list_product_categories"].function_name
-      arn        = aws_lambda_function.product_category_functions["list_product_categories"].arn
-      invoke_arn = aws_lambda_function.product_category_functions["list_product_categories"].invoke_arn
-    }
-    update_product_category = {
-      name       = aws_lambda_function.product_category_functions["update_product_category"].function_name
-      arn        = aws_lambda_function.product_category_functions["update_product_category"].arn
-      invoke_arn = aws_lambda_function.product_category_functions["update_product_category"].invoke_arn
-    }
-    delete_product_category = {
-      name       = aws_lambda_function.product_category_functions["delete_product_category"].function_name
-      arn        = aws_lambda_function.product_category_functions["delete_product_category"].arn
-      invoke_arn = aws_lambda_function.product_category_functions["delete_product_category"].invoke_arn
+    for k, v in aws_lambda_function.orders_functions : k => {
+      function_name = v.function_name
+      arn           = v.arn
+      invoke_arn    = v.invoke_arn
     }
   }
+  description = "Details of the Lambda functions created"
 }
+
+output "lambda_role_arn" {
+  value       = aws_iam_role.lambda_role.arn
+  description = "ARN of the IAM role used by the Lambda functions"
+} 

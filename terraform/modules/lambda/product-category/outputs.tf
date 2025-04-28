3c57565281cf17fed2ef36d5eb3f0b01aa8f27a3
Henrique Hartwig <henrique2805@gmail.com>
2025-04-27 22:09:10 -0300

    Update output configuration in Terraform module to reference product_category_functions instead of orders_functions, ensuring correct Lambda function details are provided.

--- terraform/modules/lambda/product-category/outputs.tf
+++ terraform/modules/lambda/product-category/outputs.tf
@@ -1,6 +1,6 @@
 output "lambda_functions" {
   value = {
-    for k, v in aws_lambda_function.orders_functions : k => {
+    for k, v in aws_lambda_function.product_category_functions : k => {
       function_name = v.function_name
       arn           = v.arn
       invoke_arn    = v.invoke_arn

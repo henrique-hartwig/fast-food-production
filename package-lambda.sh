87805c735effc8c98f4d545268b23d79de2d612c
Henrique Hartwig <henrique2805@gmail.com>
2025-04-27 21:11:32 -0300

    Add package-lambda.sh script for improved zipping of Lambda handler files. Update package.json to reference the new script, enhancing clarity and maintainability of the packaging process.

---
+++ package-lambda.sh
@@ -0,0 +1,8 @@
+mkdir -p dist
+
+for file in .build/application/handlers/**/*.js; 
+    do name=$(basename $file .js);
+    folder=$(basename $(dirname $file)); 
+    mkdir -p dist/$folder; 
+    zip -r dist/$folder/$name.zip $file .build/src .build/node_modules; 
+done

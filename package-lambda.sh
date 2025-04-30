8bd574a05c854025bed6aaf0572551a4a8e40d5d
Henrique Hartwig <henrique2805@gmail.com>
2025-04-30 00:02:24 -0300

    refactor: include additional files in Lambda function packaging for improved functionality

--- package-lambda.sh
+++ package-lambda.sh
@@ -9,6 +9,8 @@ for domain in order product product_category; do
     mkdir -p dist/$domain
     zip -r dist/$domain/$usecase.zip \
       .build/$domain/domain \
-      .build/$domain/useCases/$usecase
+      .build/$domain/useCases/$usecase \
+      .build/database/prisma/prismaClient.ts \
+      .build/utils
   done
 done

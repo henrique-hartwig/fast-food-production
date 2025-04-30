c746625a1471dcfe5949e50b8f3c72c415975d93
Henrique Hartwig <henrique2805@gmail.com>
2025-04-29 23:43:35 -0300

    refactor: correct Lambda function file paths to ensure proper referencing in Terraform configuration

--- terraform/modules/lambda/product-category/main.tf
+++ terraform/modules/lambda/product-category/main.tf
@@ -82,7 +82,7 @@ resource "aws_lambda_function" "product_category_functions" {
   role          = "arn:aws:iam::992382498858:role/LabRole"
   handler       = each.value.handler
   
-  filename         = "../../../dist/product-category/${each.key}.zip"
+  filename         = "${path.module}/../../../../dist/product-category/${each.key}.zip"
   # source_code_hash = filebase64sha256("${path.module}/../../../../.build/product-category/${each.key}.zip")
   
   layers           = [var.lambda_layer_arn]

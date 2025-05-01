d5d9a328b4b801d39a97bd840b7195d0b5b9fe91
Henrique Hartwig <henrique2805@gmail.com>
2025-04-30 22:00:38 -0300

    chore: update Lambda layer configuration to skip destruction and modify dependencies in package.json

--- terraform/modules/lambda_layer/main.tf
+++ terraform/modules/lambda_layer/main.tf
@@ -2,6 +2,6 @@ resource "aws_lambda_layer_version" "lambda_layer" {
   filename   = "${path.module}/fast-food-lambda-layer.zip"
   layer_name = "fast-food-lambda-layer"
   compatible_runtimes = ["nodejs18.x"]
-
+  skip_destroy = true
   description = "Fast Food Lambda Layer"
 }

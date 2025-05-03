54b4962cfb919f3b91a2d3d4fb8278569f88ce92
Henrique Hartwig <henrique2805@gmail.com>
2025-05-03 15:39:35 -0300

    chore: update generate_lambda_layer.sh to install production dependencies and adjust output paths for Lambda layer

--- generate_lambda_layer.sh
+++ generate_lambda_layer.sh
@@ -2,17 +2,13 @@
 
 set -e
 
-rm -rf .build
-tsc
-npm install
-
+npm install --production
 npx prisma generate --schema=./src/database/prisma/schema.prisma
 
-mkdir -p .build/nodejs
-cp -r node_modules .build/nodejs/
-cp package.json .build/nodejs/
-cp package-lock.json .build/nodejs/ 2>/dev/null || true
+cp -r node_modules ./nodejs/
+cp package.json ./nodejs/
+cp package-lock.json ./nodejs/
 
-cd .build && zip -r fastfood-lambda-layer.zip nodejs
+zip -rq fastfood-lambda-layer.zip nodejs
 
 echo "Lambda Layer gerado em fastfood-lambda-layer.zip"

654482b23dfb39af42a950b9d02457386ecd0976
Henrique Hartwig <henrique2805@gmail.com>
2025-05-03 16:25:15 -0300

    chore: enhance generate_lambda_layer.sh to display the size of the generated zip file for better visibility

--- generate_lambda_layer.sh
+++ generate_lambda_layer.sh
@@ -10,3 +10,4 @@ npx prisma generate --schema=../../../../src/database/prisma/schema.prisma
 cd .. && zip -rq fastfood-lambda-layer.zip nodejs
 
 echo "Lambda Layer gerado em fastfood-lambda-layer.zip"
+echo "Tamanho do arquivo zip: $(du -h fastfood-lambda-layer.zip | cut -f1)"

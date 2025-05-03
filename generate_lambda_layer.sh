876315d9d76f7c24e16ad7ff3d453d3a9d21c4a0
Henrique Hartwig <henrique2805@gmail.com>
2025-05-03 16:07:57 -0300

    chore: update generate_lambda_layer.sh to display files before zipping and adjust output path in Prisma schema

--- generate_lambda_layer.sh
+++ generate_lambda_layer.sh
@@ -8,7 +8,8 @@ npx prisma generate --schema=./src/database/prisma/schema.prisma
 cp -r node_modules ./nodejs/
 cp package.json ./nodejs/
 cp package-lock.json ./nodejs/
-
+echo 'MOSTRA OS ARQUIVOS'
+ls
 zip -rq fastfood-lambda-layer.zip nodejs
 
 echo "Lambda Layer gerado em fastfood-lambda-layer.zip"

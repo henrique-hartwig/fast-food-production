8091b5d7646ccdb9c2cb3273759da84153bcb5f2
Henrique Hartwig <henrique2805@gmail.com>
2025-05-03 15:19:03 -0300

    chore: update generate_lambda_layer.sh to correct zip file path for output consistency

--- generate_lambda_layer.sh
+++ generate_lambda_layer.sh
@@ -13,6 +13,6 @@ cp -r node_modules .build/nodejs/
 cp package.json .build/nodejs/
 cp package-lock.json .build/nodejs/ 2>/dev/null || true
 
-cd .build && zip -r ../fastfood-lambda-layer.zip nodejs
+cd .build && zip -r fastfood-lambda-layer.zip nodejs
 
 echo "Lambda Layer gerado em fastfood-lambda-layer.zip"

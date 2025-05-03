06e306ac2e5f0e17d592f873f373794e502d1f7d
Henrique Hartwig <henrique2805@gmail.com>
2025-05-03 14:55:23 -0300

    chore: streamline generate_lambda_layer.sh by removing unnecessary change directory command and ensuring consistent zip file creation

--- generate_lambda_layer.sh
+++ generate_lambda_layer.sh
@@ -1,7 +1,7 @@
 #bin/bash
 
 rm -rf .build
-tsc
 cd terraform/modules/lambda_layer/nodejs 
 npm install
-cd .. && zip -r fastfood-lambda-layer.zip nodejs
+cd ..
+zip -r fastfood-lambda-layer.zip nodejs

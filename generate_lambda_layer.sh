465fe823406022a61c8fd0ed9c7669978ecebd96
Henrique Hartwig <henrique2805@gmail.com>
2025-05-03 16:13:46 -0300

    chore: streamline generate_lambda_layer.sh by removing redundant copy commands and adjusting directory navigation for Lambda layer zipping

--- generate_lambda_layer.sh
+++ generate_lambda_layer.sh
@@ -2,14 +2,11 @@
 
 set -e
 
-npm install --production
+cd terraform/modules/lambda_layer/nodejs 
+
+npm install
 npx prisma generate --schema=./src/database/prisma/schema.prisma
 
-cp -r node_modules ./nodejs/
-cp package.json ./nodejs/
-cp package-lock.json ./nodejs/
-echo 'MOSTRA OS ARQUIVOS'
-ls
-zip -rq fastfood-lambda-layer.zip nodejs
+cd .. && zip -rq fastfood-lambda-layer.zip nodejs
 
 echo "Lambda Layer gerado em fastfood-lambda-layer.zip"

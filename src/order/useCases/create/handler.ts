e33b7252f6d5dba466e72da37694bcb57f25cda4
Henrique Hartwig <henrique2805@gmail.com>
2025-05-05 20:56:50 -0300

    chore: update package.json to use module type and enhance Lambda handler with PrismaClient instantiation and logging

--- src/order/useCases/create/handler.ts
+++ src/order/useCases/create/handler.ts
@@ -1,14 +1,17 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { PrismaClient } from '@prisma/client';
 import { OrderService } from '../../domain/service';
 import { DbOrderRepository } from '../../domain/database';
-import getPrismaClient from '../../../database/prisma/prismaClient';
 import { CreateOrderController } from './controller';
 import logger from '../../../utils/logger';
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const prismaClient = getPrismaClient();
+  const prismaClient = new PrismaClient();
+  console.log('prismaClient', prismaClient);
+  console.log('vai começar a lambda');
 
   try {
+    console.log('event', event);
     if (!event.body) {
       return {
         statusCode: 400,
@@ -18,13 +21,14 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
     }
 
     const requestData = JSON.parse(event.body);
+    console.log('requestData', requestData)
 
     const orderRepository = new DbOrderRepository(prismaClient);
     const orderService = new OrderService(orderRepository);
     const orderController = new CreateOrderController(orderService);
 
     const result = await orderController.handle(requestData);
-
+    console.log('result', result)
     return {
       statusCode: 201,
       headers: { 'Content-Type': 'application/json' },

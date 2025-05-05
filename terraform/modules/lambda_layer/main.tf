0fac97f1ce534c2eb6a72b4fae0139af3595f101
Henrique Hartwig <henrique2805@gmail.com>
2025-05-04 21:18:33 -0300

    fix: update Prisma Client version in package files and streamline Lambda layer zip file naming in workflows

--- terraform/modules/lambda_layer/main.tf
+++ terraform/modules/lambda_layer/main.tf
@@ -1,14 +1,14 @@
 resource "aws_lambda_layer_version" "lambda_layer_dependencies" {
-  filename   = "${path.module}/dependencies/fast-food-lambda-layer.zip"
-  layer_name = "fast-food-lambda-layer"
+  filename   = "${path.module}/dependencies/fastfood-lambda-layer.zip"
+  layer_name = "fastfood-lambda-layer"
   compatible_runtimes = ["nodejs18.x"]
   skip_destroy = true
   description = "Fast Food Lambda Layer"
 }
 
 resource "aws_lambda_layer_version" "lambda_layer_prisma" {
-  filename   = "${path.module}/prisma/fast-food-lambda-layer-prisma.zip"
-  layer_name = "fast-food-lambda-layer-prisma"
+  filename   = "${path.module}/prisma/fastfood-lambda-layer-prisma.zip"
+  layer_name = "fastfood-lambda-layer-prisma"
   compatible_runtimes = ["nodejs18.x"]
   skip_destroy = true
   description = "Fast Food Lambda Layer Prisma"
